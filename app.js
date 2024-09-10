d33f7988f50e16243d85de0a0f540e455a6a1d90
fatima mustufa <fatimamustufa6@gmail.com>
2024-09-10 11:57:01 -0700

    learn java script

---
+++ app.js
@@ -0,0 +1,34 @@
+var num1 = 10
+
+var num2 = 15
+
+var num3 = 20
+
+console.log(num1 + num2 + num3)
+
+var num1 = "10"
+
+var num2 = "15"
+
+var num3 = "20"
+
+num3 = "25"
+
+console.log(num1 + num2 + num3)
+
+
+var fname = "Fatima"
+
+var lname = "Mustufa"
+
+ console.log(fname + " " + lname)
+
+ var fname = "Fatima"
+
+ var lname = "Mustufa"
+
+ var age = 19
+
+var city = "karachi"
+
+console.log("My name is"+ " " + fname + " " + lname + ". i am" + " " + age + " " + "years old. and i lived in"+ " " + city )
